502adfaad4c6bd8ec7f75e7243529d1275c9d7fc
Symbat Dulatuly <synbat1999@gmail.com>
2025-11-29 19:24:37 +0500

    Initial commit screen shot app, bismilliah

---
+++ MyScreenShotApp/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MyScreenShotApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.hideWindowCheckbox = new System.Windows.Forms.CheckBox();
+            this.resolutionComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(112, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ScreenShot";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(123, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(112, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(214, 85);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Choose Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // hideWindowCheckbox
+            // 
+            this.hideWindowCheckbox.AutoSize = true;
+            this.hideWindowCheckbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hideWindowCheckbox.Location = new System.Drawing.Point(396, 112);
+            this.hideWindowCheckbox.Name = "hideWindowCheckbox";
+            this.hideWindowCheckbox.Size = new System.Drawing.Size(234, 43);
+            this.hideWindowCheckbox.TabIndex = 3;
+            this.hideWindowCheckbox.Text = "Hide window";
+            this.hideWindowCheckbox.UseVisualStyleBackColor = true;
+            this.hideWindowCheckbox.CheckedChanged += new System.EventHandler(this.hideWindowCheckbox_CheckedChanged);
+            // 
+            // resolutionComboBox
+            // 
+            this.resolutionComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resolutionComboBox.FormattingEnabled = true;
+            this.resolutionComboBox.Location = new System.Drawing.Point(407, 283);
+            this.resolutionComboBox.Name = "resolutionComboBox";
+            this.resolutionComboBox.Size = new System.Drawing.Size(121, 39);
+            this.resolutionComboBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resolutionComboBox);
+            this.Controls.Add(this.hideWindowCheckbox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox hideWindowCheckbox;
+        private System.Windows.Forms.ComboBox resolutionComboBox;
+    }
+}
+
---
+++ MyScreenShotApp/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyScreenShotApp
+{
+    public partial class Form1 : Form
+    {
+        private string defaultSelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        private bool hideAppWindow = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                folderDialog.Description = "Выберите папку для сохранения скриншота";
+                folderDialog.ShowNewFolderButton = true;
+
+                folderDialog.SelectedPath = defaultSelectedPath;
+
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedPath = folderDialog.SelectedPath;
+                    defaultSelectedPath = selectedPath;
+                    label1.Text = defaultSelectedPath;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = defaultSelectedPath;
+            hideWindowCheckbox.Checked = hideAppWindow;
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            int width = bounds.Width;
+            int height = bounds.Height;
+
+            resolutionComboBox.Items.Add($"Default ({width}x{height})");
+            resolutionComboBox.Items.Add($"{width / 2}x{height / 2} ");
+            resolutionComboBox.Items.Add($"{width / 4}x{height / 4} ");
+            resolutionComboBox.Items.Add($"{width / 6}x{height / 6} ");
+
+            resolutionComboBox.SelectedIndex = 0;
+        }
+
+        private Size GetSelectedResolution(Rectangle bounds) {
+            string selected = resolutionComboBox.SelectedItem.ToString();
+
+            if (selected.StartsWith("Default")) {
+                return bounds.Size;
+            }
+
+            string[] parts = selected.Split('x');
+            int w = int.Parse(parts[0]);
+            int h = int.Parse(parts[1]);
+            return new Size(w,h);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string folderPath = defaultSelectedPath;
+
+                if (!Directory.Exists(folderPath))
+                {
+                    MessageBox.Show("Указанная папка не существует!");
+                    return;
+                }
+
+                string fileName = "screenshot_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
+                string fullPath = Path.Combine(folderPath, fileName);
+
+                Rectangle bounds = Screen.GetBounds(Point.Empty);
+                if (hideAppWindow) {
+                    this.Hide();
+                    System.Threading.Thread.Sleep(300);
+                }
+                Size targetSize = GetSelectedResolution(bounds);
+
+                using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+                {
+                    using (Graphics g = Graphics.FromImage(bitmap))
+                    {
+                        g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                    }
+
+                    using (Bitmap resized = new Bitmap(bitmap, targetSize))
+                    {
+                        resized.Save(fullPath, ImageFormat.Png);
+                    }
+                    //bitmap.Save(fullPath, ImageFormat.Png);
+                }
+
+                if (hideAppWindow) {
+                    this.Show();
+                }
+
+                MessageBox.Show("Скриншот сохранен: " + fullPath);
+            }
+
+            catch (Exception ex) {
+                MessageBox.Show("Ошибка при сохранении скриншота: " + ex.Message);
+            }
+        }
+
+        private void hideWindowCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            hideAppWindow = hideWindowCheckbox.Checked;
+        }
+    }
+}
